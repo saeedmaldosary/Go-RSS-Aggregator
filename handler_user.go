d450921255987fd51cb386cc9b69aa8e8923c25c
Saeed Aldosary <SAldosary@Scai.sa>
2023-08-20 16:32:01 +0300

    set api keys for the users

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/saeedmaldosary/Go-RSS-Aggregator/internal/auth"
 	"github.com/saeedmaldosary/Go-RSS-Aggregator/internal/database"
 )
 
@@ -32,6 +33,23 @@ func (apiCfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Reque
 
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Couldn't get user: %v", err))
 		return
 	}
 
