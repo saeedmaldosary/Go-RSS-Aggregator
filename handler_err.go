74fb3f73a49a2206db69eb5a154342b024ff5d42
Saeed Aldosary <SAldosary@Scai.sa>
2023-08-20 09:07:37 +0300

    fix error response

--- handler_err.go
+++ handler_err.go
@@ -4,5 +4,5 @@ import "net/http"
 
 func handlerErr(w http.ResponseWriter, r *http.Request) {
 
-	respondWithJSON(w, 400, "Something went wrong")
+	respondWithError(w, 400, "Something went wrong")
 }
